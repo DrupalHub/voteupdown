4026dad0c767e30478b2dc9c0eb36aa008e41795
Roy Segall <roysegall@dsl6740u.home>
2014-02-27 22:00:45 +0200

    Moving most of the work to the base class.

--- plugins/widgets/thumbs/thumbs.js
+++ plugins/widgets/thumbs/thumbs.js
@@ -1,16 +1,19 @@
-
+/**
+ * Thumbs widget JS file.
+ */
 (function ($) {
 
-Drupal.behaviors.vud_widget_thumbs = {
-  attach: function (context) {
-    if (!$('.vud-widget').hasClass('vud-widget-processed')) {
-      $('.vote-thumb').click(function(){
-        if($(this).hasClass('up-active') || $(this).hasClass('down-active')){
-          $(this).parents('.vud-widget').find('.vud-link-reset').click();
-        }
-      });
+  Drupal.behaviors.vud_widget_thumbs = {
+    attach: function () {
+
+      if (!$('.vud-widget').hasClass('vud-widget-processed')) {
+        $('.vote-thumb').click(function() {
+          if($(this).hasClass('up-active') || $(this).hasClass('down-active')) {
+            $(this).parents('.vud-widget').find('.vud-link-reset').click();
+          }
+        });
+      }
     }
-  }
-};
+  };
 
 })(jQuery);
