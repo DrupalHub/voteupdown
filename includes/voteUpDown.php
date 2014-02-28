03397d5335a4cf7bf30252e41a82193e7707d059
Roy Segall <roysegall@Roys-MacBook-Pro-2.local>
2014-02-28 13:01:44 +0200

    Set the votes via the entity metadata wrapper.

--- includes/voteUpDown.php
+++ includes/voteUpDown.php
@@ -5,7 +5,15 @@ class VoteUpDown {
   /**
    * Get all the votes for a specific entity.
    */
-  public static function getResults($entity_type, $entity_id) {
+  public static function getResults($entity_type, $entity) {
+    list($id) = entity_extract_ids($entity_type, $entity);
+
+    return db_select('votingapi_vote')
+      ->fields('votingapi_vote')
+      ->condition('entity_id', $id)
+      ->condition('entity_type', $entity_type)
+      ->execute()
+      ->fetchAllAssoc('vote_id');
   }
 
   /**
